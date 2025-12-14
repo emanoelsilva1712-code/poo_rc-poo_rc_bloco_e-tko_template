9f2ed7c9287d2c6b14cbc7be93efd8cb99bfbee1
Emanoel Silva <emanoelsilva1712@gmail.com>
2025-12-14 20:43:35 +0000

    2 horas

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -1,9 +1,5 @@
-class Estacionamento:
-    def __init__(self, veiculo: str, horaAtual: int):
-        self.veiculo = veiculo
-        self.horaAtual = horaAtual
-
 from abc import ABC, abstractmethod
+
 class Veiculo(ABC):
     def __init__(self, id: str, tipo: str, horaEntrada: int):
         self.id = id
@@ -11,34 +7,62 @@ class Veiculo(ABC):
         self.horaEntrada = horaEntrada
 
     @abstractmethod
-    def calcularValor(self):
+    def calcularValor(self, horaEntrada: int) -> float:
         pass
 
+    def __str__(self) -> str:
+        tipo_fomatado = self.tipo.rjust(10, '_')
+        id_formatado = self.id.rjust(10, '_')
+
+        return f"{tipo_fomatado} : {id_formatado} : {self.horaEntrada}"
+
 class Bike(Veiculo):
-    def __init__(self, id: str, tipo: str, horaEntrada: int):
-        super().__init__(id, tipo, horaEntrada)
+    def __init__(self, id: str, horaEntrada: int):
+        super().__init__(id, "Bike", horaEntrada)
 
-    def CalcularValor(self):
-
+    def CalcularValor(self, horaSaida: int) -> float:
+        return 3.00    #valor fixo
 
 class Moto(Veiculo):
-    def __init__(self, id: str, tipo: str, horaEntrada: int):
-        super().__init__(id, tipo, horaEntrada)
+    def __init__(self, id: str, horaEntrada: int):
+        super().__init__(id, "Moto", horaEntrada)
 
-    def CalcularValor(self):
+    def CalcularValor(self, horaSaida: int) -> float:
+        tempo_estacionado = horaSaida - self.horaEntrada
 
+        if tempo_estacionado <= 0:
+            return 0.00
+
+        valor = tempo_estacionado / 20.0
+        return valor
 
 class Carro(Veiculo):
-    def __init__(self, id: str, tipo: str, horaEntrada: int):
-        super().__init__(id, tipo, horaEntrada)
+    def __init__(self, id: str, horaEntrada: int):
+        super().__init__(id, "Carro", horaEntrada)
 
-    def CalcularValor(self):
+    def CalcularValor(self, horaSaida: int) -> float:
+        tempo_estacionado = horaSaida - self.horaEntrada
+
+        if tempo_estacionado <= 0:
+            return 5.00
+        
+        valor = tempo_estacionado / 10.0
+        return max(valor, 5.00)
+
+class Estacionamento:
+    def __init__(self, veiculo: str, horaAtual: int):
+        self.veiculo = veiculo
+        self.horaAtual = horaAtual
 
 def main():
 
     estacionamento = Estacionamento()
     while True:
-        line = input()
+        try:
+            line = input()
+        except IOError:
+            break
+
         print("$" + line)
         args = line.split()        
 
@@ -48,4 +72,11 @@ def main():
         elif args[0] == "show":
             print(estacionamento)
 
+        elif args[0] == "tempo":
+
+        elif args[0] == "estacionar":
+
+        elif args[0] == "pagar":
+
+main()
 
