f1c4dcbfb6a42c70d5bd40537bcd08c83c50acf0
Emanoel Silva <emanoelsilva1712@gmail.com>
2025-12-15 01:59:30 +0000

    2 horas

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import math
 
 class Veiculo(ABC):
     def __init__(self, id: str, tipo: str, horaEntrada: int):
@@ -20,14 +21,14 @@ class Bike(Veiculo):
     def __init__(self, id: str, horaEntrada: int):
         super().__init__(id, "Bike", horaEntrada)
 
-    def CalcularValor(self, horaSaida: int) -> float:
+    def calcularValor(self, horaSaida: int) -> float:
         return 3.00    #valor fixo
 
 class Moto(Veiculo):
     def __init__(self, id: str, horaEntrada: int):
         super().__init__(id, "Moto", horaEntrada)
 
-    def CalcularValor(self, horaSaida: int) -> float:
+    def calcularValor(self, horaSaida: int) -> float:
         tempo_estacionado = horaSaida - self.horaEntrada
 
         if tempo_estacionado <= 0:
@@ -40,7 +41,7 @@ class Carro(Veiculo):
     def __init__(self, id: str, horaEntrada: int):
         super().__init__(id, "Carro", horaEntrada)
 
-    def CalcularValor(self, horaSaida: int) -> float:
+    def calcularValor(self, horaSaida: int) -> float:
         tempo_estacionado = horaSaida - self.horaEntrada
 
         if tempo_estacionado <= 0:
@@ -50,9 +51,60 @@ class Carro(Veiculo):
         return max(valor, 5.00)
 
 class Estacionamento:
-    def __init__(self, veiculo: str, horaAtual: int):
-        self.veiculo = veiculo
-        self.horaAtual = horaAtual
+    def __init__(self):
+        self.veiculosEstacionados: dict[str, Veiculo] = {}
+        self.horaAtual = 0
+
+    def setTempo(self, tempo: int):
+        self.horaAtual += tempo
+
+    def estacionar(self, tipo: str, id: str):
+        if id in self.veiculosEstacionados:
+            print(f"Erro: veiculo {id} já está está estacionado.")
+            return
+        
+        hora_entrada = self.horaAtual
+        if tipo.lower() == "bike":
+            veiculo = Bike(id, hora_entrada)
+        elif tipo.lower() == "moto":
+            veiculo = Moto(id, hora_entrada)
+        elif tipo.lower() == "carro":
+            veiculo = Carro(id, hora_entrada)
+        else:
+            print(f"Erro: Tipo de veiculos {tipo} desconhecido.")
+            return 
+        
+        self.veiculosEstacionados[id] = veiculo
+
+    def pagar(self, id: str):
+        if id not in self.veiculosEstacionados:
+            print(f"Veiculo {id} não está estacionado.")
+            return 
+
+        veiculo = self.veiculosEstacionados.pop(id)
+        hora_saida = self.horaAtual
+        valor = veiculo.calcularValor(hora_saida)
+        print(f"{veiculo.tipo} chegou {veiculo.horaEntrada} saiu {hora_saida}. Pagar R$ {valor:.2f}")
+
+    def __str__(self):
+        output = ""
+
+        def ordem_tipo(tipo):
+            if tipo == "Bike":
+                return 1
+            elif tipo == "Moto":
+                return 2
+            elif tipo =="Carro":
+                return 3
+            return 99
+
+        veiculos_ordenados = sorted(self.veiculosEstacionados.values(), key=lambda v: (ordem_tipo(v.tipo), v.id))
+
+        for veiculo in veiculos_ordenados:
+            output += f"{veiculo}\n"
+
+        output += f"Hora atual: {self.horaAtual}"
+        return output.strip()
 
 def main():
 
@@ -73,10 +125,28 @@ def main():
             print(estacionamento)
 
         elif args[0] == "tempo":
+            if len(args) == 2:
+                try:
+                    tempo = int(args[1])
+                    estacionamento.setTempo(tempo)
+                except ValueError:
+                    print("Erro: Tempo invalido.")
 
         elif args[0] == "estacionar":
+            if len(args) == 3:
+                tipo = args[1]
+                id = args[2]
+                estacionamento.estacionar(tipo, id)
+            else:
+                print("Erro: Uso: estacionar <tipo> id>")
 
         elif args[0] == "pagar":
+            if len(args) == 2:
+                id = args[1]
+                estacionamento.pagar(id)
+            else:
+                print("Erro: Uso: pagar <id>")
+
 
 main()
 
